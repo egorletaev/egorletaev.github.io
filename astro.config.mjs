3ddf4158c2826970b21eee7587520166721d2059
egorletaev <yegorletaev@gmail.com>
2025-06-25 16:58:24 +0300

    tailwind has been installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://egorletaev.github.io',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
